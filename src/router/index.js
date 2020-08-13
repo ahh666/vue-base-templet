ba196fdf121109b5a4c39edc5b99162320964ff5
unknown <aihh26979@hs.handsome.com.cn>
2020-08-13 17:20:07 +0800

    路由跳转处理

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,22 @@
+/*
+ * @Description: 前端路由
+ * @Author: 艾欢欢
+ * @Date: 2020-07-23 16:30:18
+ * @LastEditTime: 2020-08-06 16:18:12
+ * @LastEditors: 艾欢欢
+ * @FilePath: \vue-base-templet\src\router\index.js
+ */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 // 为了首屏加载速度，首屏不需要按需加载
 import Home from '../views/home'
+
+// 处理路由跳转会报错的问题: Error: Avoided redundant navigation to current location: xxx
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch((err) => err)
+}
 
 Vue.use(VueRouter)
 
