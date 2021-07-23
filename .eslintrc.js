03ad02253f65ba192b4b4af60c43d08f7abc69bd
aihuanhuan <44722523+ahh666@users.noreply.github.com>
2021-07-23 14:00:44 +0800

    增加格式化规范注释

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@
  * @Description: Description
  * @Author: 艾欢欢
  * @Date: 2020-07-24 11:17:07
- * @LastEditTime: 2021-07-16 18:42:47
+ * @LastEditTime: 2021-07-23 13:58:54
  * @LastEditors: 艾欢欢<ahh666@qq.com>
  * @FilePath: \vue-base-templet\.eslintrc.js
  */
@@ -37,22 +37,22 @@ module.exports = {
     'prettier/prettier': [
       'error',
       {
-        eslintIntegration: true,
-        arrowParens: 'avoid',
-        bracketSpacing: true,
+        eslintIntegration: true, // 让prettier使用eslint的代码格式进行校验
+        arrowParens: 'avoid', //  (x) => {} 箭头函数参数只有一个时是否要有小括号。avoid：省略括号
+        bracketSpacing: true, // 在对象，数组括号与文字之间加空格 "{ foo: bar }"
         htmlWhitespaceSensitivity: 'ignore',
         insertPragma: false,
-        jsxBracketSameLine: false,
-        jsxSingleQuote: false,
-        printWidth: 120,
-        proseWrap: 'preserve',
+        jsxBracketSameLine: false, // 在jsx中把'>' 是否单独放一行
+        jsxSingleQuote: false, // 在jsx中使用单引号代替双引号
+        printWidth: 120, // 超过最大值换行
+        proseWrap: 'preserve', // 默认值。因为使用了一些折行敏感型的渲染器（如GitHub comment）而按照markdown文本样式进行折行
         quoteProps: 'as-needed',
         requirePragma: false,
-        semi: false,
-        singleQuote: true,
-        tabWidth: 2,
-        trailingComma: 'all',
-        useTabs: false,
+        semi: false, // 句尾不添加分号
+        singleQuote: true, // 使用单引号代替双引号
+        tabWidth: 2, // 缩进字节数
+        trailingComma: 'all', // 在对象或数组最后一个元素后面是否加逗号
+        useTabs: false, // 缩进不使用tab，使用空格
       },
     ],
     // 使用2个空格缩进
